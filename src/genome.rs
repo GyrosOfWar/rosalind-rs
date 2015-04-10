58ecae9aa24df3d3b77e8f8be4b449c06dc14010
Martin Tomasi <martin.tomasi@gmail.com>
2015-04-10 12:20:27 +0200

    started work on finding protein motif

--- src/genome.rs
+++ src/genome.rs
@@ -7,26 +7,22 @@ use std::fs::File;
 use std::collections::{HashMap, HashSet};
 use std::str::from_utf8_unchecked;
 
+/// Counts the number of occurrences of the characters A, G, C and T in the input string.
 pub fn count_nucleotides(input: &str) -> (usize, usize, usize, usize) {
-    let mut a_count = 0;
-    let mut g_count = 0;
-    let mut c_count = 0;
-    let mut t_count = 0;
-    
-    for ch in input.chars() {
-        match ch {
-            'A' => a_count += 1,
-            'G' => g_count += 1,
-            'C' => c_count += 1,
-            'T' => t_count += 1,
-            '\n' | '\t' | ' ' | '\r' => {},
-            p => panic!("Invalid character: {}", p)
-        }
-    }
-
-    (a_count, c_count, g_count, t_count)
+    input.chars()
+        .fold((0, 0, 0, 0),
+              |(a, g, c, t), ch| {
+                  match ch {
+                      'A' => (a+1, g, c, t),
+                      'G' => (a, g+1, c, t),
+                      'C' => (a, g, c+1, t),
+                      'T' => (a, g, c, t+1),
+                      _ => (a, g, c, t)
+                  }
+              })
 }
-
+/// Returns a table mapping codons (three nucleotides) to proteins.
+/// The stop codons maps to the character '_'.
 pub fn dna_codon_table() -> HashMap<&'static str, char> {
     let data = include_str!("../dna_codon_table.txt");
     let mut table = HashMap::new();
@@ -47,7 +43,7 @@ pub fn dna_codon_table() -> HashMap<&'static str, char> {
 pub fn dna_to_rna(input: &str) -> String {
     input.replace("T", "U")
 }
-
+/// Returns the reverse complement of a string.
 pub fn reverse_complement(input: &str) -> String {
     input.chars().rev().map(|ch| match ch {
         'A' => 'T',
@@ -57,7 +53,8 @@ pub fn reverse_complement(input: &str) -> String {
         c => c
     }).collect()
 }             
-
+/// Parses the FASTA file at the given path and returns
+/// the data stored in it as a map from label to data point.
 pub fn parse_fasta(path: &str) -> HashMap<String, String> {
     let mut result: HashMap<String, String> = HashMap::new();
     let file = BufReader::new(File::open(Path::new(path)).unwrap());
@@ -111,14 +108,15 @@ pub fn protein_weights(protein_string: &str, table: HashMap<char, f64>) -> f64 {
         .map(|c| table[&c])
         .fold(0.0, |sum, x| sum + x)
 }
-
+/// Translates a reading frame from the given DNA string to a protein string. If
+/// the given string has no start codon or does not have an end codon anywhere,
+/// returns None. 
 pub fn reading_frame(string: &str, dna_codon_table: &HashMap<&str, char>) -> Option<String> {
-    let bytes = string.as_bytes();
     if !string.starts_with("ATG") {
         None
     } else {
         let mut value = String::new();
-        for codon_bytes in bytes.chunks(3) {
+        for codon_bytes in string.as_bytes().chunks(3) {
             let codon = unsafe { from_utf8_unchecked(codon_bytes) };
 
             if let Some(&dna) = dna_codon_table.get(codon) {
@@ -133,7 +131,7 @@ pub fn reading_frame(string: &str, dna_codon_table: &HashMap<&str, char>) -> Opt
         None
     }
 }
-
+/// Finds all the open reading frames in the given DNA string and returns them in a set. 
 pub fn open_reading_frames(dna_string: &str, dna_codon_table: HashMap<&str, char>) -> HashSet<String> {
     let mut result = HashSet::new();
     let reverse_complement = reverse_complement(dna_string);
@@ -149,10 +147,78 @@ pub fn open_reading_frames(dna_string: &str, dna_codon_table: HashMap<&str, char
     result
 }
 
-pub fn longest_common_substring(strings: Vec<&str>) -> &str {
-    let mut max_len = 0;
-    let mut longest_str: &str;
+pub struct ProteinMotif<'a> {
+    source: &'a str,
+    motif: Vec<Motif>,
+    idx: usize
+}
+
+impl<'a> ProteinMotif<'a> {
+    pub fn new(src: &'a str) -> ProteinMotif<'a> {
+        ProteinMotif {
+            source: src,
+            motif: vec![],
+            idx: 0
+        }
+    }
+
+    fn consume_char(&mut self) -> char {
+        let mut iter = self.source[self.idx..].char_indices();
+        let (_, cur_char) = iter.next().unwrap();
+        let (next_pos, _) = iter.next().unwrap_or((1, ' '));
+        self.idx += next_pos;
+        return cur_char;
+    }
+
+    fn peek_char(&self) -> char {
+        self.source.chars().next().unwrap()
+    }
+
+    fn eof(&self) -> bool {
+        self.idx >= self.source.len()
+    }
+
+    fn parse_either(&mut self) -> Motif {
+        let mut chars = Vec::new();
+        let limit = 5;
+        for _ in 0..limit {
+            match self.consume_char() {
+                ']' => return Motif::Either(chars),
+                c => chars.push(c) 
+            }
+        }
+
+        panic!("Invalid either string")
+    }
+
+    fn parse_not(&mut self) -> Motif {
+        let ch = self.consume_char();
+        assert!(self.consume_char() == '}');
+        Motif::Not(ch)
+    }
+    
+    pub fn parse(&mut self) -> Vec<Motif> {
+        while !self.eof() {
+            let current = self.consume_char();
+            let token = match current {
+                '[' => self.parse_either(),
+                '{' => self.parse_not(),
+                _ => Motif::Char(current)
+            };
+            self.motif.push(token);
+        }
+
+        self.motif.clone()
+    }
+}
+#[derive(Debug, Clone)]
+pub enum Motif {
+    Char(char),
+    Either(Vec<char>),
+    Not(char)
+}
 
 
+pub fn find_protein_motif(motif: &str, data: &str) -> Vec<usize> {
     unimplemented!()
 }
